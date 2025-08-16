786727c5e1a454125f000a6992eb5dbff6548cd8
Dolapo Akindolire <seundolapo00@gmail.com>
2025-08-16 13:57:23 +0100

    initial commit

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,39 @@
+const MainContent = () => {
+  return (
+    <main>
+      <div className="next_event">
+        <h1>METHATHESIS 2025</h1>
+        <p>25 DAYS</p>
+        <a href="./event_details" className="event_details">
+          Click For Details
+        </a>
+      </div>
+
+      <section className="event_list">
+        <h2>Upcoming Events</h2>
+        <div className="event">
+          <div>January 26 - January 30 2025</div>
+          <div>Methathesis</div>
+          <div>
+            <input type="button" value="Click for details" />
+          </div>
+        </div>
+        <div className="event">
+          <div>January 26 - January 30 2025</div>
+          <div>Methathesis</div>
+          <div>
+            <input type="button" value="Click for details" />
+          </div>
+        </div>
+        <div className="event">
+          <div>January 26 - January 30 2025</div>
+          <div>Methathesis</div>
+          <div>
+            <input type="button" value="Click for details" />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+export default MainContent;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+const NavBar = () => {
+  return (
+    <div>
+      <header>
+        <h1>Event Manager</h1>
+        <a
+          href="./admin_login_page"
+          target="_blank"
+          className="admin_login_btn"
+        >
+          Admin Login
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default NavBar;

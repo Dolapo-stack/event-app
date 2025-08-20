ac2f41901db434ede4b101b37b1193338c4c3998
Dolapo Akindolire <seundolapo00@gmail.com>
2025-08-20 11:24:02 +0100

    update eventcard

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,43 +1,16 @@
-import Methathesis from "../assets/methathesis.jpeg";
-
-const EventCard = () => {
+const EventCard = (props) => {
   return (
     <>
-      <div className="event-card-list">
-        <div className="event">
-          <img src={Methathesis} alt="methathesis" />
-          <h2 className="title"> METHATHESIS</h2>
-          <p className="date">20th-24th August, 2025</p>
-          <p className="venue">Trem Int'l Hqtrs, Lagos</p>
-          <input
-            type="button"
-            value="Click for details"
-            className="details_button"
-          />
-        </div>
-
-        <div className="event">
-          <img src={Methathesis} alt="methathesis" />
-          <h2 className="title"> METHATHESIS</h2>
-          <p className="date">20th-24th August, 2025</p>
-          <p className="venue">Trem Int'l Hqtrs, Lagos</p>
-          <input
-            type="button"
-            value="Click for details"
-            className="details_button"
-          />
-        </div>
-        <div className="event">
-          <img src={Methathesis} alt="methathesis" />
-          <h2 className="title"> METHATHESIS</h2>
-          <p className="date">20th-24th August, 2025</p>
-          <p className="venue">Trem Int'l Hqtrs, Lagos</p>
-          <input
-            type="button"
-            value="Click for details"
-            className="details_button"
-          />
-        </div>
+      <div className="event">
+        <img src={props.image} alt="methathesis" />
+        <h2 className="title"> {props.title}</h2>
+        <p className="date">{props.date}</p>
+        <p className="venue">{props.venue}</p>
+        <input
+          type="button"
+          value="Click for details"
+          className="details_button"
+        />
       </div>
     </>
   );
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,4 +1,7 @@
 import EventCard from "./EventCard";
+import Methathesis from "../assets/methathesis.jpeg";
+import WorldCelebration from "../assets/word_celebration.jpeg";
+import KLWC from "../assets/kingdom_life_world_conference.jpeg";
 
 const MainContent = () => {
   return (
@@ -13,7 +16,26 @@ const MainContent = () => {
 
       <section className="event_list">
         <h2>Upcoming Events</h2>
-        <EventCard />
+        <div className="event-card-list">
+          <EventCard
+            image={Methathesis}
+            title="Methathesis"
+            date="20th-24th August, 2025"
+            venue="Trem Int'l Hqtrs, Lagos"
+          />
+          <EventCard
+            image={WorldCelebration}
+            title="Word Celebration"
+            date="18th-21st April, 2026"
+            venue="Trem Int'l Hqtrs, Lagos"
+          />
+          <EventCard
+            image={KLWC}
+            title="Kingdom Life World Conference"
+            date="12th-16th November, 2024"
+            venue="Trem Int'l Hqtrs, Lagos"
+          />
+        </div>
       </section>
     </main>
   );

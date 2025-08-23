72faf2e6416f2a6286805513bcee3eaa523ed411
Dolapo Akindolire <seundolapo00@gmail.com>
2025-08-23 22:09:43 +0100

    update

---
+++ a.js
@@ -0,0 +1,7 @@
+const names = ["alice", "bob", "calum", "dave"];
+const capitalizeFirstLetter = names.map((name) => {
+  return;
+  name[0].toUpperCase();
+});
+
+console.log(capitalizeFirstLetter);
